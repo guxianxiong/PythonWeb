4c8ba7f01883a06dc2e6e52bb70fd94c423d2224
guxianxiong <guxianxiong@163.com>
2016-06-17 18:06:24 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -21,9 +21,27 @@ class HomePageTest(TestCase):
 		request.POST['item_text'] = 'A new list item'
 
 		response = home_page(request)
-		self.assertIn('A new list item', response.content.decode())
 
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
 
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_only_saves_items_when_neccessary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
 
 class ItemModelTest(TestCase):
 	def test_saving_and_retrieving_items(self):
@@ -36,7 +54,7 @@ class ItemModelTest(TestCase):
 		second_item.save()
 
 		save_items = Item.objects.all()
-		self.assertEqual(saved_items.count(), 2)
+		self.assertEqual(save_items.count(), 2)
 
 		first_saved_item = save_items[0]
 		second_saved_item = save_items[1]
