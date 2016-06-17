3b28a9bfd4c2b414ca8e1449c4523d30ee0dd6a8
guxianxiong <guxianxiong@163.com>
2016-06-17 15:46:31 +0800

    Model for list Items and associated migration

--- functional_tests.py
+++ functional_tests.py
@@ -1,15 +1,21 @@
 from selenium import webdriver
 import unittest
 from selenium.webdriver.common.keys import Keys
+import time
 
 class NewVisitorTest(unittest.TestCase):
+
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 	def tearDown(self):
-		# self.browser.quit()
-		pass
+		self.browser.quit()
 		
+		
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		self.browser.get('http://localhost:8000')
@@ -24,18 +30,30 @@ class NewVisitorTest(unittest.TestCase):
 			'Enter a to-do item'
 			)
 
-		inputbox.send_keys('Buy peacock feathers')
+		inputbox.send_keys('1: Buy peacock feathers')
+		inputbox.send_keys(Keys.ENTER)
+		self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-		inputbox.send_keys('Keys.ENTER')
+		# inputbox = self.browser.find_element_by_id('id_new_item')
+		# inputbox.send_keys('2: Use peacock feathers to make a fly')
+		# inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
-			)
+		# self.check_for_row_in_list_table('1: Buy peacock feathers')
+		# self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+		
 
 		self.fail('Finish the test!')
 
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
-		
+	
+
+
+
+
+
+
+
+
+
